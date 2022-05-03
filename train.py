913e68b7dbb40a3decfeb53fa0138eb02d2033a7
Simon Boeger <simonboeger@dhcp-39-71-7.dhcpint.csulb.edu>
2022-05-03 14:46:57 -0700

    added notes file to gitignore

---
+++ train.py
@@ -0,0 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="yo")
+parser.add_argument("", )
+
+'''
+arguments
+    --dataset
+    actually how are we gonna do that dataset?
+'''
